ef027247f98c4cb44d8275664426c243000dc759
chaos037 <ccllkk3377@hotmail.com>
2015-03-19 14:11:48 +0800

    lawl

--- app/src/main/java/com/mad/shinypenny/AddNewBudget.java
+++ app/src/main/java/com/mad/shinypenny/AddNewBudget.java
@@ -1,22 +1,30 @@
 package com.mad.shinypenny;
 
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.mad.shinypenny.Logic.BudgetDataSource;
 import com.mad.shinypenny.data.BudgetCategory;
 import com.mad.shinypenny.data.RecurringItem;
 import com.mad.shinypenny.data.TransactionItem;
 
+import java.util.Date;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 
@@ -26,11 +34,13 @@ public class AddNewBudget extends ActionBarActivity {
     Spinner spnCycleCategory;
     private BudgetDataSource budgetDataSource;
     EditText edtBudgetAmount;
-
-
+    EditText budgetStartDate;
+    EditText budgetEndDate;
+    SimpleDateFormat dateFormatter;
+    private DatePickerDialog startDatePickerDialog;
+    private DatePickerDialog endDatePickerDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        edtBudgetAmount = (EditText) findViewById(R.id.newBudgetAmount);
 
         budgetDataSource = new BudgetDataSource(this);
         try {
@@ -41,8 +51,20 @@ public class AddNewBudget extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
 
+        edtBudgetAmount = (EditText) findViewById(R.id.newBudgetAmount);
+
+        budgetStartDate = (EditText) findViewById(R.id.budgetStartDate);
+        budgetStartDate.setInputType(InputType.TYPE_NULL);
+        budgetStartDate.requestFocus();
+
+        budgetEndDate = (EditText) findViewById(R.id.budgetEndDate);
+        budgetEndDate.setInputType(InputType.TYPE_NULL);
+        budgetEndDate.requestFocus();
+        dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+
         initializeBudgetSpinner();
         initializeCycleSpinner();
+        setDateTimeField();
     }
 
     public void initializeBudgetSpinner() {
@@ -59,10 +81,36 @@ public class AddNewBudget extends ActionBarActivity {
 
     public void initializeCycleSpinner() {
         spnCycleCategory = (Spinner) findViewById(R.id.budgetCycleSpinner);
-        String[] cycleItem = {"Only Once","Daily", "Weekly", "Bi-Weekly", "Monthly", "Yearly"};
+        String[] cycleItem = {"Only Once", "Daily", "Weekly", "Bi-Weekly", "Monthly", "Yearly"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cycleItem);
         spnCycleCategory.setAdapter(adapter);
         spnCycleCategory.setSelection(0);
+    }
+
+    private void setDateTimeField() {
+        Calendar newCalendar = Calendar.getInstance();
+        startDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                budgetStartDate.setText(dateFormatter.format(newDate.getTime()));
+                endDatePickerDialog.getDatePicker().setMinDate(newDate.getTimeInMillis());
+
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        endDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                budgetEndDate.setText(dateFormatter.format(newDate.getTime()));
+                startDatePickerDialog.getDatePicker().setMaxDate(newDate.getTimeInMillis());
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
     }
 
 
@@ -88,20 +136,49 @@ public class AddNewBudget extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
+
+
+    public void addNewBudget(View view) throws Exception{
+        RecurringItem newRecurringItem = new RecurringItem();
+        TransactionItem newTransactionItem;
+        double amount = 0.0;
+        Date startDate;
+        Date endDate;
+        String cycle;
+        String category;
+        spnBudgetCategory.getSelectedItemPosition();
+
+        cycle = spnCycleCategory.getSelectedItem().toString();
+        amount = Double.parseDouble(edtBudgetAmount.getText().toString());
+        startDate = dateFormatter.parse(budgetStartDate.getText().toString());
+        endDate = dateFormatter.parse(budgetEndDate.getText().toString());
+
+        if(cycle.equals("Only Once")){
+            newTransactionItem = new TransactionItem();
+            newTransactionItem.setID("");
+            newTransactionItem.getCategoryID();
+        }else{
+
+
+        }
+
+//        BudgetCategory budget = new BudgetCategory(budgetId.getText().toString(),budgetName.getText().toString());
+//        long l = budgetDataSource.createBudget(budget);
+//        if(l!=-1) {
+//            Log.e(null, String.valueOf( l));
+//            budgetId.setText(null);
+//            budgetName.setText(null);
+//        }
 
     }
 
-    public void addNewBudget(View view) {
-        RecurringItem newRecurringItem = new RecurringItem();
-        TransactionItem newTransactionItem = new TransactionItem();
 
-        spnBudgetCategory.getSelectedItem().toString();
-        spnCycleCategory.getSelectedItem().toString();
-        edtBudgetAmount.getText().toString();
+    public void promptStartDate(View view) {
+        startDatePickerDialog.show();
+    }
 
+    public void promptEndDate(View view) {
+        endDatePickerDialog.show();
     }
 }
 
